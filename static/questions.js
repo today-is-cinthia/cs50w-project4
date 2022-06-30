f5ff9235f07fde355f8b35d8cedbb2a5a3a43c13
cinthia <cinthiaduarte013@gmail.com>
2022-06-30 11:41:44 -0600

    quiz responsive

--- static/questions.js
+++ static/questions.js
@@ -90,24 +90,24 @@ let questions8 = [
   },
   {
     numb: 9,
-    question: "¿QUÉ REGALO LE HIZO EL PASTELERO DOMINIQUE PERSOONE A KEITH RICHARDS?    ",
-    answer: "Un artilugio para esnifar chocolate    ",
+    question: "Which of the following is NOT a physical sign of attraction?",
+    answer: "Constricted pupils",
     options: [
-      "Una guitarra hecha de chocolate      ",
-      "Un bombón de chocolate y marihuana al que llamó 'Le petit Keith'      ",
-      "Un artilugio para esnifar chocolate      ",
-      "Una fuente de chocolate de 3 metros de alto para sus conciertos      "
+      "Dilatated pupils",
+      "Flushed cheeks",
+      "Sweating palms",
+      "Constricted pupils"
     ]
   },
   {
     numb: 10,
-    question: "¿DE QUÉ COLOR ES LA SEMILLA DEL CACAO?    ",
-    answer: "Blanca",
+    question: "What chemical is responsible for making our hearts race when we see someone who tickles our fancy? ",
+    answer: "Norepinephrine",
     options: [
-      "Cafe",
-      "Blanca",
-      "Negro",
-      "Verde"
+      "Dopamine",
+      "Norepinephrine",
+      "Adrenaline",
+      "I don´t know"
     ]
   },
 ];
@@ -222,6 +222,120 @@ let questions5 = [
     "Blanca",
     "Negro",
     "Verde"
+  ]
+},
+];
+
+
+let questions7 = [
+  {
+  numb: 1,
+  question: " How many chromosome pairs make up the human genome?",
+  answer: "23",
+  options: [
+    "24",
+    "23",
+    "12",
+    "18"
+  ]
+},
+  {
+  numb: 2,
+  question: "Your genes could influence your choice of partner, true or false?",
+  answer: "True",
+  options: [
+    "I don´t know",
+    "True",
+    "False",
+    "Hello"
+  ]
+},
+  {
+  numb: 3,
+  question: "The molecular structure of DNA was first discovered by which two of the following?",
+  answer: "Francis Crick",
+  options: [
+    "Isaac Newton    ",
+    " James Watson    ",
+    "Francis Crick",
+    " Blackpink  "
+  ]
+},
+  {
+  numb: 4,
+  question: "A nucleic acid sequence is a succession of letters that indicate the order of nucleotides within a DNA or RNA molecule. When was the first complete nucleotide sequence of a genome sequenced? ",
+  answer: "1976",
+  options: [
+    "1976",
+    "1995",
+    "2007",
+    "2004"
+  ]
+},
+  {
+  numb: 5,
+  question: "Wheat has one of the most challenging genomes. Common bread wheat is hexaploid, meaning each nucleus contains how many individual genomes? ",
+  answer: "Three",
+  options: [
+    "Two",
+    "Four",
+    "Seven",
+    "Three"
+  ]
+},
+{
+  numb: 6,
+  question: "Genomic characterization of tumors for diseases like ESCC is likely to reveal additional oncogenic mechanisms. What is ESSC? ",
+  answer: " Esophageal squamous cell carcinoma",
+  options: [
+    " Esophageal squamous cell carcinoma",
+    " Esophageal squamous cell cancer    ",
+    " Esophageal Stage III cell carcinoma    ",
+    "What in the world is that lol"
+  ]
+},
+{
+  numb: 7,
+  question: "The nucleosome is the fundamental subunit of chromatin, consisting of a segment of DNA wrapped around eight histone protein cores. Approximately how many base pairs make up the nucleosome?",
+  answer: "146",
+  options: [
+    "146",
+    "246",
+    " 346    ",
+    "142"
+  ]
+},
+{
+  numb: 8,
+  question: "In population genetics, what does MAF stand for?  ",
+  answer: "Minor allele frequency",
+  options: [
+    " Mitochondrial assisted finishing    ",
+    " Macrophage-activating function    ",
+    "Minor allele frequency",
+    "What?"
+  ]
+},
+{
+  numb: 9,
+  question: "Rapid whole genome sequencing (WGS) has proven to be very useful in the diagnosis of genetic diseases in very ill infants. How fast can we sequence a newborn’s genome?",
+  answer: "26 hours",
+  options: [
+    "2 days",
+    "10 hours",
+    "26 hours",
+    "8 hours"
+  ]
+},
+{
+  numb: 10,
+  question: "Fill in the missing words: The DNA methylation mark in ________ is transmitted from one generation to the next, propagating epigenetic information in a much more direct route compared to __________.",
+  answer: "Plants, animals",
+  options: [
+    "Plants, animals",
+    "Animals, plants",
+    "I'm hungry",
+    "What?"
   ]
 },
 ];
--- static/quiz.js
+++ static/quiz.js
@@ -105,24 +105,39 @@ next_btn.onclick = ()=>{
             clearInterval(counterLine);
             showResult(); //showResult function
         }}
+        if(id == 7){
+            if(que_count < questions7.length - 1){
+                que_count++; 
+                que_numb++; 
+                showQuetions(que_count); 
+                queCounter(que_numb); 
+                clearInterval(counter); 
+                clearInterval(counterLine); 
+                startTimer(timeValue); 
+                startTimerLine(widthValue); 
+                timeText.textContent = "Time Left"; 
+                next_btn.classList.remove("show"); 
+            }else{
+                clearInterval(counter); 
+                clearInterval(counterLine); 
+                showResult(); //showResult function
+            }}
 }
 
 
 function showQuetions(index){
     const que_text = document.querySelector(".que_text");
 if(id == 5){
-    //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>'+ questions5[index].numb + ". " + questions5[index].question +'</span>';
     let option_tag = '<div class="option"><span>'+ questions5[index].options[0] +'</span></div>'
     + '<div class="option"><span>'+ questions5[index].options[1] +'</span></div>'
     + '<div class="option"><span>'+ questions5[index].options[2] +'</span></div>'
     + '<div class="option"><span>'+ questions5[index].options[3] +'</span></div>';
-    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
-    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag; 
     
     const option = option_list.querySelectorAll(".option");
 
-    // set onclick attribute to all available options
     for(i=0; i < option.length; i++){
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
@@ -133,6 +148,22 @@ if(id == 8){
     + '<div class="option"><span>'+ questions8[index].options[1] +'</span></div>'
     + '<div class="option"><span>'+ questions8[index].options[2] +'</span></div>'
     + '<div class="option"><span>'+ questions8[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag; 
+    
+    const option = option_list.querySelectorAll(".option");
+
+    // onclick para todas las opcionees
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+if(id == 7){
+    let que_tag = '<span>'+ questions7[index].numb + ". " + questions7[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions7[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions7[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions7[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions7[index].options[3] +'</span></div>';
     que_text.innerHTML = que_tag; 
     option_list.innerHTML = option_tag; 
     
@@ -212,45 +243,56 @@ if(id == 8){
     }
     next_btn.classList.add("show"); 
 }
+if(id == 7){
+    clearInterval(counter); 
+    clearInterval(counterLine); 
+    let userAns = answer.textContent; 
+    let correcAns = questions7[que_count].answer; 
+    const allOptions = option_list.children.length; 
+    
+    if(userAns == correcAns){ 
+        userScore += 1; 
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); 
+        answer.insertAdjacentHTML("beforeend", crossIconTag); 
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ 
+                option_list.children[i].setAttribute("class", "option correct"); 
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                alert("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.classList.add("show"); 
+}
 }
 
 function showResult(){
-    if(id == 5){
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
     if (userScore > 6){ // si el usuario obtuvo mas de 6 correctas
-        let scoreTag = '<span>Opa!, You got <p>'+ userScore +'</p> out of <p>'+ questions8.length +'🤩</p></span>';
+        let scoreTag = '<span>Opa!, You got <p>'+ userScore +'</p> out of <p>'+ questions7.length +'🤩</p></span>';
         scoreText.innerHTML = scoreTag;  
     }
     else if(userScore > 3){ // si el usuario obtuvo mas de 3
-        let scoreTag = '<span>Nice, You got <p>'+ userScore +'</p> out of <p>'+ questions8.length +'🤪</p></span>';
+        let scoreTag = '<span>Nice, You got <p>'+ userScore +'</p> out of <p>'+ questions7.length +'🤪</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     else{ // si el usuario obtuvo menos o 1 respuesta correcta
-        let scoreTag = '<span>Yikes, You got only <p>'+ userScore +'</p> out of <p>'+ questions8.length +'☠️🥱</p></span>';
+        let scoreTag = '<span>Yikes, You got only <p>'+ userScore +'</p> out of <p>'+ questions7.length +'☠️🥱</p></span>';
         scoreText.innerHTML = scoreTag;
     }
-}
-if(id == 8){
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.remove("activeQuiz"); //hide quiz box
-    result_box.classList.add("activeResult"); //show result box
-    const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 6){ // si el usuario obtuvo mas de 6 correctas
-        let scoreTag = '<span>Opa!, You got <p>'+ userScore +'</p> out of <p>'+ questions8.length +'🤩</p></span>';
-        scoreText.innerHTML = scoreTag;  
-    }
-    else if(userScore > 3){ // si el usuario obtuvo mas de 3
-        let scoreTag = '<span>Nice, You got <p>'+ userScore +'</p> out of <p>'+ questions8.length +'🤪</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-    else{ // si el usuario obtuvo menos o 1 respuesta correcta
-        let scoreTag = '<span>Yikes, You got only <p>'+ userScore +'</p> out of <p>'+ questions8.length +'☠️🥱</p></span>';
-        scoreText.innerHTML = scoreTag;
-    }
-}
 }
 
 function startTimer(time){
@@ -295,6 +337,21 @@ function startTimer(time){
             }
             next_btn.classList.add("show"); //muestra el boton siguiente
         }
+        if(id == 7){
+            let correcAns = questions7[que_count].answer; //extrae respuesta correcta de la lista
+            for(i=0; i < allOptions; i++){
+                //añade color e icono a la respuesta correcta autoseleccionada
+                if(option_list.children[i].textContent == correcAns){ 
+                    option_list.children[i].setAttribute("class", "option correct"); 
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                    alert("Time Off: Auto selected correct answer.");
+                }
+            }
+            for(i=0; i < allOptions; i++){
+                option_list.children[i].classList.add("disabled"); //cuando el usuario selecciona una opcion deshabilota las demas
+            }
+            next_btn.classList.add("show"); //muestra el boton siguiente
+        }
 
     }
     }
@@ -321,4 +378,8 @@ function queCounter(index){
         let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions8.length +'</p> Questions</span>';
         bottom_ques_counter.innerHTML = totalQueCounTag;  
         }
+        if(id == 7){
+            let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions7.length +'</p> Questions</span>';
+            bottom_ques_counter.innerHTML = totalQueCounTag;  
+            }
 }
